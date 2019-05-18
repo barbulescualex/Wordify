ead73f255231b7d73eaffcb1a3d410c905712a39
Alex Barbulescu <alexbarbulescu01@gmail.com>
2019-05-18 16:18:04 -0400

    added vertical random placement

--- Wordify/Helpers/Grid.swift
+++ Wordify/Helpers/Grid.swift
@@ -22,25 +22,34 @@ class Grid{
         
         randomizeArray(&self.wordSet)
         
-        addWordHelper()
+        addWords()
         fillRest()
 
         return words
     }
     
+    private func addWords(){
+        for word in self.wordSet {
+            addWord(string: word)
+        }
+    }
+    
     private func addWord(string: String){
         //random direction to place the word in
-        let direction = Direction.horizontal
+        let direction = Direction(rawValue: Int.random(in: 0...1)) ?? .horizontal
         let charArray = string.map({$0})
         let sizeOfWord = charArray.count
         
-        var c = false
         if direction == .horizontal {
+            //constraints on y axis
             let minY = 0
             let maxY = sideLength - 1
             
+            //constraints on x axis
             let minX = 0
             var maxX = sideLength - sizeOfWord - 1
+            
+            
             var maxLoops = maxY*maxX
             
             if maxX < 0 {
@@ -51,7 +60,6 @@ class Grid{
             }
             
             var placed = false
-            
             
             var charCells = [CharCell]()
             
@@ -94,13 +102,70 @@ class Grid{
                 print("couldn't place: ", string)
             }
         }
+        
+        if direction == .vertical {
+            let minX = 0
+            let maxX = sideLength - 1
+            
+            let minY = 0
+            var maxY = sideLength - sizeOfWord - 1
+            
+            var maxLoops = maxY*maxX
+            
+            if maxY < 0 {
+                maxY = 0
+            }
+            if maxY == 0 {
+                maxLoops = maxX
+            }
+            
+            var placed = false
+            
+            var charCells = [CharCell]()
+            
+            while(!placed && maxLoops != 0){
+                maxLoops -= 1
+                let candidateX = Int.random(in: minX...maxX)
+                let candidateY = Int.random(in: minY...maxY)
+                
+                var indexInCellArray = candidateY*sideLength + candidateX
+                var couldPlace = true
+                
+                for (i,char) in charArray.enumerated() {
+                    let cell = cellArray[indexInCellArray]
+                    if (cell.char == nil) {
+                        cell.char = char //add the word char in
+                        charCells.append(cell) //add it to the charCells
+                        indexInCellArray += 10
+                        continue
+                    }
+                    if (cell.char == char){
+                        charCells.append(cell) //we're good with the overlap
+                        indexInCellArray += 10
+                        continue
+                    }
+                    //else conflict, try new candidate placement
+                    charCells = []//clear
+                    couldPlace = false
+                    break
+                }
+                
+                if couldPlace {
+                    //everything was placed sucessfully
+                    placed = true
+                    let word = Word(string: string, cells: charCells, found: false)
+                    words.append(word)
+                } //else loop again with new random position
+            }
+            
+            if placed == false {
+                print("couldn't place: ", string)
+            }
+        }
+        
+        
     }
     
-    private func addWordHelper(){
-        for word in self.wordSet {
-            addWord(string: word)
-        }
-    }
     
     
     //MARK: Helpers
